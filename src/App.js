3cc3c5cc9a5976bb2a70f260177dbbd209936bcd
Terrance <terrance888@proton.me>
2022-06-21 20:23:51 -0400

    Add category item and category menu components with seperate style sheets. Integrate with main App.js. Install SASS

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
+import CategoryMenu from "./components/category-menu/category-menu.component";
+
 
 const App = () => {
 
-
   return (
-    <div className="categories-container">
-      <div className="category-container">
-        {/*<img />*/}
-        <div className="category-body-container">
-          <h2>Hats</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      
-    </div>
+    <CategoryMenu />
   );
 };
 
