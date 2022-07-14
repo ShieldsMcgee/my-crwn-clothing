113d970637ee1ebeba0eeca081a08f57ee15b89d
Terrance <terrance888@proton.me>
2022-07-14 18:12:55 -0400

    update cart context with reducer method

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -2,6 +2,8 @@ import { createContext, useEffect, useReducer } from 'react';
 
 import { onAuthStateChangedListener, createUserDocumentFromAuth } from '../utils/firebase/firebase.utils';
 
+import { createAction } from '../utils/reducer/reducer.utils';
+
 export const UserContext = createContext({
     currentUser: null,
     setCurrentUser: () => null,
@@ -12,8 +14,6 @@ export const USER_ACTION_TYPES = {
 }
 
 const userReducer = (state, action) => {
-    console.log('dispatched');
-    console.log(action);
     const { type, payload } = action;
 
     switch (type) {
@@ -31,10 +31,9 @@ const INITIAL_STATE = {
 }
 export const UserProvider = ({ children }) => {
     const [{currentUser}, dispatch] = useReducer(userReducer, INITIAL_STATE);
-    console.log(currentUser);
 
     const setCurrentUser = (user) => {
-        dispatch({type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user})
+        dispatch(createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user));
     }
 
     const value = { currentUser, setCurrentUser };
@@ -53,14 +52,3 @@ export const UserProvider = ({ children }) => {
     return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
 
 }
-
-/*
-
-const userReducer = (state, action) => {
-    return {
-        currentUser: {}
-    }
-
-}
-
-*/
