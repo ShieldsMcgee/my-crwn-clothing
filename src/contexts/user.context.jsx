821d97c2073322cb4db37e3c662512c71df06e2a
Terrance <terrance888@proton.me>
2022-07-14 14:18:36 -0400

    update user context from useState to useReducer

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,19 +1,47 @@
-import {createContext, useState, useEffect} from 'react';
+import { createContext, useEffect, useReducer } from 'react';
 
-import { onAuthStateChangedListener, createUserDocumentFromAuth  } from '../utils/firebase/firebase.utils';
+import { onAuthStateChangedListener, createUserDocumentFromAuth } from '../utils/firebase/firebase.utils';
 
 export const UserContext = createContext({
     currentUser: null,
     setCurrentUser: () => null,
 })
 
-export const UserProvider = ({ children}) => {
-    const [currentUser, setCurrentUser] = useState(null);
-    const value = {currentUser, setCurrentUser};
+export const USER_ACTION_TYPES = {
+    SET_CURRENT_USER: 'SET_CURRENT_USER'
+}
+
+const userReducer = (state, action) => {
+    console.log('dispatched');
+    console.log(action);
+    const { type, payload } = action;
+
+    switch (type) {
+        case USER_ACTION_TYPES.SET_CURRENT_USER:
+            return {
+                currentUser: payload
+            }
+        default:
+            throw new Error(`Unhandled type ${type} in userReducer`);
+    }
+}
+
+const INITIAL_STATE = {
+    currentUser: null
+}
+export const UserProvider = ({ children }) => {
+    const [{currentUser}, dispatch] = useReducer(userReducer, INITIAL_STATE);
+    console.log(currentUser);
+
+    const setCurrentUser = (user) => {
+        dispatch({type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user})
+    }
+
+    const value = { currentUser, setCurrentUser };
 
     useEffect(() => {
         const unsubscribe = onAuthStateChangedListener((user) => {
-            if(user) {
+            if (user) {
                 createUserDocumentFromAuth(user);
             }
             setCurrentUser(user);
@@ -25,3 +53,14 @@ export const UserProvider = ({ children}) => {
     return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
 
 }
+
+/*
+
+const userReducer = (state, action) => {
+    return {
+        currentUser: {}
+    }
+
+}
+
+*/
